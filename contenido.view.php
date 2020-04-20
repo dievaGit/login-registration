2921bb9316584d40c58dd0fd58b3ef393a13fd4f
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-20 14:53:24 -0400

    views

---
+++ contenido.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
+   <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+   <link rel="stylesheet" href="css/styles.css">
+   <title>Contenido</title>
+  
+</head>
+<body>
+	<div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+        <hr class="border">
+        <div class="contenido">
+          <article>
+          	<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text</p>
+
+          	<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text</p>
+          </article>
+        </div>	
+	</div>	
+
+    
+     
+</body>
+</html>	
